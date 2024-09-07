7dffaacc2f84d9d0fdc5e493df4ee293926d5688
Jonny Gomez <jonnygomez@unitec.edu>
2024-09-06 18:17:46 -0600

    FINAL

--- Lab7_JonnyGomez/Ejercicio1.h
+++
@@ -1,9 +0,0 @@
-#ifndef EJERCICIO1_H
-#define EJERCICIO1_H
-#include <QFile>
-#include <QTextStream>
-
-
-
-
-#endif // EJERCICIO1_H
--- Lab7_JonnyGomez/cframe.h
+++ Lab7_JonnyGomez/cframe.h
@@ -2,6 +2,8 @@
 #define CFRAME_H
 
 #include <QMainWindow>
+#include <Producto.h>
+#include <Pelicula.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,13 +18,32 @@ class cframe : public QMainWindow
 public:
     cframe(QWidget *parent = nullptr);
     ~cframe();
+    QVector<Producto> leerProductosDesdeArchivo(const QString& nombreArchivo);
+    void guardarProductosEnArchivo(const QString& nombreArchivo, const QVector<Producto>& productos);
+    void guardarProductosEnArchivo(const QString& nombreArchivo);
+    void crearArchivoBinarioDePrueba();
+    void guardarEstadisticasEnArchivo(const QString& archivo, double promedioCalificaciones, const QMap<int, int>& peliculasPorAno);
 
 private slots:
     void on_pushButton_4_clicked();
 
     void on_generar_reporte_clicked();
 
+    void on_botonActualizar_clicked();
+
+    void on_botonCargarProductos_2_clicked();
+
+    void on_botonCargarProductos_clicked();
+
+    void on_botonGenerar3_clicked();
+
+    void on_botonCargarArchivo_clicked();
+
+    void on_botonEstadisticas_clicked();
+
 private:
     Ui::cframe *ui;
+    QVector<Producto> listaProductos;
+    QVector<Pelicula> peliculas;
 };
 #endif // CFRAME_H
